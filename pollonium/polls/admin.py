390291f912c8bdaf340551e7e3e58e0b96832038
manuargue <manu.argue@gmail.com>
2017-08-17 21:44:23 -0300

    add templates. modify models. add static css/img. modified admin polls

--- pollonium/polls/admin.py
+++ pollonium/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poll, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General',     {'fields': ['title', 'location', 'description', 'pub_date', 'author', 'author_email']}),
+        ('Settings',    {'fields': ['hidden_poll', 'single_vote', 'limit_votes', 'votes_max']}),
+        ('Sharing',     {'fields': ['only_invited']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('title', 'pub_date', 'author')
+    list_filter = ['pub_date']
+    search_fields = ['title']
+
+
+admin.site.register(Poll, PollAdmin)
--- pollonium/polls/models.py
+++ pollonium/polls/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+FIELD_DESC = {
+    'pub_date':     'published date',
+    'single_vote':  'limit participants to a single vote',
+    'limit_votes':  'limit the number of votes per option',
+    'votes_max':    'votes per option',
+    'only_invited': 'only invited people can see the poll',
+}
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(FIELD_DESC['pub_date'])
+    location = models.CharField(default='', blank=True, max_length=20)
+    description = models.TextField(default='', blank=True)
+    author = models.CharField(max_length=20)
+    author_email = models.EmailField()
+    limit_votes = models.BooleanField(FIELD_DESC['limit_votes'], default=False)
+    votes_max = models.PositiveIntegerField(FIELD_DESC['votes_max'], default=0)
+    hidden_poll = models.BooleanField(default=False)
+    single_vote = models.BooleanField(FIELD_DESC['single_vote'], default=False)
+    only_invited = models.BooleanField(FIELD_DESC['only_invited'], default=False)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now-datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.text
